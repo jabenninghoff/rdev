1be347c900762472a205f8d4dfbdb494b143e6eb
John Benninghoff <jbenninghoff@mac.com>
2021-09-29 11:27:52 -0500

    Move style_all and lint_all to ci

--- R/ci.R
+++ R/ci.R
@@ -1,3 +1,57 @@
+#' Style all files
+#'
+#' Style all files in a project. Implemented as a wrapper for [styler::style_dir()] that styles `.R`
+#'  and `.Rmd` filetypes, and excludes `.git`, `.github`, `packrat`, and `renv` by default.
+#'
+#' @param path Path to a directory with files to transform.
+#' @param exclude_dirs Character vector with directories to exclude
+#'   (recursively).
+#' @param filetype Vector of file extensions indicating which file types should
+#'   be styled. Case is ignored, and the `.` is optional, e.g.
+#'   `c(".R", ".Rmd")`, or `c("r", "rmd")`. Supported values (after
+#'   standardization) are: "r", "rprofile", "rmd", "rnw".
+#' @examples
+#' \dontrun{
+#' style_all()
+#' style_all("notebooks")
+#' style_all(
+#'   path = "./inst",
+#'   filetype = "r",
+#'   exclude_dirs = c("inst/example/bad.R")
+#' )
+#' }
+#' @export
+# add tests using local_create_package() per https://testthat.r-lib.org/articles/test-fixtures.html
+style_all <- function(path = ".", filetype = c("R", "Rprofile", "Rmd"),
+                      exclude_dirs = c(".git", ".github", "packrat", "renv")) {
+  styler::style_dir(path = path, filetype = filetype, exclude_dirs = exclude_dirs)
+}
+
+#' Lint all files
+#'
+#' Lint all files in a project. Implemented as a wrapper for [lintr::lint_dir()] that excludes
+#' `.git`, `.github`, `packrat`, and `renv` by default.
+#'
+#' @param path the path to the base directory, by default,
+#'   it will be searched in the parent directories of the current directory.
+#' @param exclusions exclusions for [lintr::exclude()], relative to the
+#'   package path.
+#' @return A list of lint objects.
+#' @examples
+#' \dontrun{
+#' lint_all()
+#' lint_all("notebooks")
+#' lint_all(
+#'   path = "./inst",
+#'   exclusions = list("inst/example/bad.R")
+#' )
+#' }
+#' @export
+# add tests using local_create_package() per https://testthat.r-lib.org/articles/test-fixtures.html
+lint_all <- function(path = ".", exclusions = list(".git", ".github", "packrat", "renv")) {
+  lintr::lint_dir(path = path, exclusions = exclusions)
+}
+
 #' Local CI
 #'
 #' Run continuous integration tests locally.
