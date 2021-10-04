f3fa61480f7897305f7d07b2362c6105b5e5f95f
John Benninghoff <jbenninghoff@mac.com>
2021-10-03 21:45:52 -0500

    Update defaults, inherit styler documentation

--- R/ci.R
+++ R/ci.R
@@ -1,30 +1,19 @@
 #' Style all files
 #'
-#' Style all files in a project. Implemented as a wrapper for [styler::style_dir()] that styles `.R`
-#'  and `.Rmd` filetypes, and excludes `.git`, `.github`, `packrat`, and `renv` by default.
+#' Style all files in a project. Implemented as a wrapper for [styler::style_dir()] that styles
+#'   `.R`, `.Rprofile`, `.Rmd`, and `.Rnw` files by default.
 #'
-#' @param path Path to a directory with files to transform.
-#' @param exclude_dirs Character vector with directories to exclude
-#'   (recursively).
-#' @param filetype Vector of file extensions indicating which file types should
-#'   be styled. Case is ignored, and the `.` is optional, e.g.
-#'   `c(".R", ".Rmd")`, or `c("r", "rmd")`. Supported values (after
-#'   standardization) are: "r", "rprofile", "rmd", "rnw".
+#' @inheritParams styler::style_dir
+#' @inheritDotParams styler::style_dir
 #' @examples
 #' \dontrun{
 #' style_all()
-#' style_all("notebooks")
-#' style_all(
-#'   path = "./inst",
-#'   filetype = "r",
-#'   exclude_dirs = c("inst/example/bad.R")
-#' )
+#' style_all("analysis", filetype = "Rmd")
 #' }
 #' @export
 # add tests using local_create_package() per https://testthat.r-lib.org/articles/test-fixtures.html
-style_all <- function(path = ".", filetype = c("R", "Rprofile", "Rmd"),
-                      exclude_dirs = c(".git", ".github", "packrat", "renv")) {
-  styler::style_dir(path = path, filetype = filetype, exclude_dirs = exclude_dirs)
+style_all <- function(path = ".", filetype = c("R", "Rprofile", "Rmd", "Rnw"), ...) {
+  styler::style_dir(path = path, filetype = filetype, ...)
 }
 
 #' Lint all files
