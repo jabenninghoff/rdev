85500bbc9cc9c16082ebfcf5febedb7cebe8d8e5
John Benninghoff <jbenninghoff@mac.com>
2021-12-26 11:16:07 -0600

    Add parameters for disabling grid lines

--- R/viridis_quo.R
+++ R/viridis_quo.R
@@ -6,6 +6,8 @@
 #' Quo requires [Lato](https://www.latofonts.com), which can be installed on macOS using
 #'   `brew install font-lato`.
 #'
+#' @param major,major.x,major.y,minor,minor.x,minor.y set to FALSE to disable grid lines by setting
+#'   the appropriate panel.grid element to [ggplot2::element_blank()].
 #' @export
 #'
 #' @examples
@@ -30,13 +32,29 @@
 #'     color = "Gears"
 #'   ) +
 #'   theme_quo()
-theme_quo <- function() {
-  ggplot2::theme_minimal(base_family = "Lato") +
+# nolint start: snake_case_linter
+theme_quo <- function(major = TRUE,
+                      major.x = TRUE,
+                      major.y = TRUE,
+                      minor = TRUE,
+                      minor.x = TRUE,
+                      minor.y = TRUE) {
+  # nolint end
+  gg <- ggplot2::theme_minimal(base_family = "Lato") +
     ggplot2::theme(
       plot.title = ggplot2::element_text(face = "bold"),
       plot.subtitle = ggplot2::element_text(face = "bold"),
       plot.caption = ggplot2::element_text(hjust = 0)
     )
+
+  if (!major) gg <- gg + ggplot2::theme(panel.grid.major = ggplot2::element_blank())
+  if (!major.x) gg <- gg + ggplot2::theme(panel.grid.major.x = ggplot2::element_blank())
+  if (!major.y) gg <- gg + ggplot2::theme(panel.grid.major.y = ggplot2::element_blank())
+  if (!minor) gg <- gg + ggplot2::theme(panel.grid.minor = ggplot2::element_blank())
+  if (!minor.x) gg <- gg + ggplot2::theme(panel.grid.minor.x = ggplot2::element_blank())
+  if (!minor.y) gg <- gg + ggplot2::theme(panel.grid.minor.y = ggplot2::element_blank())
+
+  gg
 }
 
 #' Viridis Quo
