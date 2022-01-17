8114f578b175b0c3f7ddde768a7c63daaaa0bb83
John Benninghoff <jbenninghoff@mac.com>
2022-01-16 19:34:30 -0600

    Commit partial implementation of release_stage()

--- R/release.R
+++ R/release.R
@@ -53,3 +53,45 @@ get_release <- function(pkg = ".", filename = "NEWS.md") {
 
   list(version = version, notes = notes)
 }
+
+#' Stage a GitHub release
+#'
+#' Open a GitHub pull request for a new release from `NEWS.md`. Approve, merge, and create the
+#'   release using `release_merge()`.
+#'
+#' When run, `release_stage()`:
+#' 1. Extracts release version and release notes from `NEWS.md` using [get_release()]
+#' 1. Validates release notes and version
+#'
+#' @param pkg Path to package. Currently, only `pkg = "."` is supported.
+#' @param filename name of file containing release notes, defaults to `NEWS.md`.
+#'
+#' @export
+release_stage <- function(pkg = ".", filename = "NEWS.md") {
+  if (pkg != ".") {
+    stop('currently only build_analysis_site(pkg = ".") is supported')
+  }
+
+  rel <- get_release(pkg = pkg, filename = filename)
+
+  # enforce specific semantic version pattern (1.0.0)
+  if (!grepl("^[0-9]*\\.[0-9]*\\.[0-9]*$", rel$version)) {
+    stop("invalid package version: '", rel$version, "'")
+  }
+  if (length(rel$notes[rel$notes != ""]) < 1) {
+    stop("no release notes found!")
+  }
+  # stop if version tag already exists
+  if (nrow(gert::git_tag_list(match = rel$version, repo = pkg)) > 0) {
+    stop("release tag '", rel$version, "' already exists!")
+  }
+
+  # stop if any uncommitted changes
+  # create new branch if on default branch, otherwise use current branch
+  # update Version in DESCRIPTION
+  # commit DESCRIPTION with message: "<label> release <version>"
+  # run build_analysis_site() or build_rdev_site()
+  # commit changes with message: "<function> for <label> release <version>"
+  # push changes
+  # open pull request, "<package> <version>", body = release notes
+}
