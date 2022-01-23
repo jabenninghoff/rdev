c25228a2323818824b2441cffb134d4147e5e610
John Benninghoff <jbenninghoff@mac.com>
2022-01-23 16:28:10 -0600

    Add tests for sort_file(), sort_rbuildignore()

---
+++ tests/testthat/test-sort_file.R
@@ -0,0 +1,20 @@
+test_that("sort_file sorts a file", {
+  tmp_file <- withr::local_tempfile()
+
+  strings <- stringi::stri_rand_strings(10, 10)
+  writeLines(strings, tmp_file)
+  sort_file(tmp_file)
+
+  expect_equal(readLines(tmp_file), sort(strings))
+})
+
+test_that("sort_rbuildignore sorts .Rbuildignore", {
+  withr::local_dir(withr::local_tempdir())
+  withr::local_file(".Rbuildignore")
+
+  strings <- stringi::stri_rand_strings(10, 10)
+  writeLines(strings, ".Rbuildignore")
+  sort_rbuildignore()
+
+  expect_equal(readLines(".Rbuildignore"), sort(strings))
+})
