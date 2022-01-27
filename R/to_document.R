20d09f5b504e851ead8dc830ce9c4a7478059902
John Benninghoff <jbenninghoff@mac.com>
2022-01-27 13:00:14 -0600

    Separate code for to_document()

---
+++ R/to_document.R
@@ -0,0 +1,59 @@
+#' Convert R Notebook to `html_document`
+#'
+#' Copies a file using [fs::file_copy()] and changes the output type in the yaml front matter from
+#'   `html_notebook` to `html_document`, removing all other output types.
+#'
+#' @param file_path Path to the source file
+#' @param new_path Path to copy the converted file using [fs::file_copy()]
+#' @param overwrite Overwrite file if it exists, passed to [fs::file_copy()]
+#'
+#' @return Path to new file
+#'
+#' @seealso [build_analysis_site()]
+#' @examples
+#' \dontrun{
+#' to_document("notebook.Rmd", "document.Rmd")
+#' to_document("notebooks_dir/notebook.Rmd", "documents_dir")
+#' }
+#' @export
+to_document <- function(file_path, new_path, overwrite = FALSE) {
+  if (!(fs::path_ext(file_path) %in% c("Rmd", "rmd"))) {
+    stop("file_path, '", file_path, "' is not an R Markdown (*.Rmd) file!")
+  }
+
+  notebook <- readLines(file_path)
+  header <- grep("^---$", notebook)
+  yaml <- rmarkdown::yaml_front_matter(file_path)
+  if (length(header) < 2 | length(yaml) < 1) {
+    stop("file_path, '", file_path, "' is not a valid R Notebook!")
+  }
+
+  if (is.character(yaml$output)) {
+    if (yaml$output != "html_notebook") {
+      stop("file_path, '", file_path, "' does not contain `output: html_notebook`!")
+    }
+    yaml$output <- "html_document"
+  } else if (is.list(yaml$output)) {
+    if (is.null(yaml$output$html_notebook)) {
+      stop("file_path, '", file_path, "' does not contain `output: html_notebook`!")
+    }
+    yaml$output <- list(html_document = yaml$output$html_notebook)
+  } else {
+    stop("unexpected object type for output: '", typeof(yaml$output), "'")
+  }
+
+  body_start <- header[2] + 1
+  body_end <- length(notebook)
+  nb_body <- notebook[body_start:body_end]
+
+  notebook <- c(
+    "---",
+    gsub("\\n$", "", yaml::as.yaml(yaml)),
+    "---",
+    nb_body
+  )
+
+  new_file <- fs::file_copy(file_path, new_path, overwrite = overwrite)
+  writeLines(notebook, new_file)
+  return(new_file)
+}
