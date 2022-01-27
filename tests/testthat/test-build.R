ef2f080576f3ea91a5040707ec528bdbf4436118
John Benninghoff <jbenninghoff@mac.com>
2022-01-26 19:03:43 -0600

    Annotate build_rdev_site()

--- tests/testthat/test-build.R
+++ tests/testthat/test-build.R
@@ -1,5 +1,35 @@
 withr::local_dir("test-build")
 
+# build_rdev_site
+
+test_that('build_rdev_site errors when pkg is something other than "."', {
+  mockery::stub(build_rdev_site, "devtools::build_readme", NULL)
+  mockery::stub(build_rdev_site, "pkgdown::clean_site", NULL)
+  mockery::stub(build_rdev_site, "pkgdown::build_site", NULL)
+
+  expect_error(
+    build_rdev_site(pkg = "test"), 'currently only build_analysis_site\\(pkg = "."\\) is supported'
+  )
+  expect_output(build_rdev_site(pkg = "."))
+})
+
+test_that("all build_rdev_site functions are called", {
+  mockery::stub(build_rdev_site, "devtools::build_readme", NULL)
+  mockery::stub(build_rdev_site, "pkgdown::clean_site", NULL)
+  mockery::stub(build_rdev_site, "pkgdown::build_site", NULL)
+
+  expect_output(
+    build_rdev_site(),
+    paste0(
+      "^(?s)devtools::build_readme\\(\\)\\n\\npkgdown::clean_site\\(\\)\\n\\n",
+      "pkgdown::build_site\\(\\)$"
+    ),
+    perl = TRUE
+  )
+})
+
+# to_document
+
 test_that("to_document errors when file isn't a well-formed R markdown document", {
   dest <- fs::file_temp(pattern = "notebook", ext = "Rmd")
   withr::local_file(dest)
