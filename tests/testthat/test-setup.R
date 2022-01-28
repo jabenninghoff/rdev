6af2c02dbebf1873a1417d191c6772ea923f7a16
John Benninghoff <jbenninghoff@mac.com>
2022-01-27 23:06:22 -0600

    Start test coverage for setup.R

---
+++ tests/testthat/test-setup.R
@@ -0,0 +1,9 @@
+# use_package_r
+test_that("use_package_r creates an R directory if it doesn't exist", {
+  withr::local_dir(withr::local_tempdir())
+  mockery::stub(use_package_r, "usethis::use_template", NULL)
+
+  use_package_r(open = FALSE)
+
+  expect_equal(fs::dir_exists("R"), c(R = TRUE))
+})
