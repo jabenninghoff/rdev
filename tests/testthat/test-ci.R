a5c4cf4ea85f0f5cc953635f503f3862c2d770ab
John Benninghoff <jbenninghoff@mac.com>
2022-01-27 22:07:11 -0600

    Move check_renv() tests to ci

--- tests/testthat/test-ci.R
+++ tests/testthat/test-ci.R
@@ -1,5 +1,33 @@
 withr::local_dir("test-ci")
 
+# check_renv
+
+test_that("All renv functions are called", {
+  mockery::stub(check_renv, "renv::status", NULL)
+  mockery::stub(check_renv, "renv::clean", NULL)
+  mockery::stub(check_renv, "renv::update", NULL)
+
+  expect_output(
+    check_renv(),
+    "^(?s)renv::status\\(\\)\\n\\nrenv::clean\\(\\)\\n\\nrenv::update\\(\\)$",
+    perl = TRUE
+  )
+})
+
+test_that("renv::update isn't run when update = FALSE", {
+  mockery::stub(check_renv, "renv::status", NULL)
+  mockery::stub(check_renv, "renv::clean", NULL)
+  mockery::stub(check_renv, "renv::update", NULL)
+
+  expect_output(
+    check_renv(update = FALSE),
+    "^(?s)renv::status\\(\\)\\n\\nrenv::clean\\(\\)$",
+    perl = TRUE
+  )
+})
+
+# style_all
+
 # set styler.quiet = FALSE to suppress output
 style_test <- withr::with_options(list(styler.quiet = TRUE), style_all())
 
@@ -14,6 +42,8 @@ test_that("style_all returns the correct columns", {
 test_that("style_all tests R and Rmd files", {
   expect_equal(nrow(style_test), 3)
 })
+
+# lint_all
 
 # testthat suppresses messages but because lintr uses interactive() to control message output, there
 #   is no easy way to turn off the side-effect of ci being printed twice in the test() report,
@@ -34,6 +64,8 @@ test_that("lint_all checks all test files", {
   skip_on_ci()
   expect_snapshot(lint_all(), cran = FALSE)
 })
+
+# ci
 
 test_that("All renv functions are called only when parameter = TRUE", {
   mockery::stub(ci, "style_all", NULL)
