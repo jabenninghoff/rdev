37b5ffb6108278f522f0f92aa45171621fd36191
John Benninghoff <jbenninghoff@mac.com>
2022-01-28 20:18:05 -0600

    Update rmd_metadata()

--- tests/testthat/test-to_document.R
+++ tests/testthat/test-to_document.R
@@ -98,3 +98,59 @@ test_that("to_document copies source file to a directory", {
   expect_null(doc_yaml$output$html_notebook)
   expect_equal(doc_yaml$output$html_document, nb_yaml$output$html_notebook)
 })
+
+# rmd_metadata
+
+test_that("rmd_metadata errors when file isn't a well-formed R markdown document", {
+  mockery::stub(rmd_metadata, "desc::desc_get_urls", "https://example.github.io/package")
+
+  expect_error(rmd_metadata("test.txt"), "is not an R Markdown \\(\\*\\.Rmd\\) file")
+  expect_error(rmd_metadata("no-front-matter.Rmd"), "is not a valid R Notebook")
+  expect_error(rmd_metadata("no-yaml.Rmd"), "is not a valid R Notebook")
+})
+
+test_that("rmd_metadata errors when yaml front matter doesn't contain `html_notebook`", {
+  mockery::stub(rmd_metadata, "desc::desc_get_urls", "https://example.github.io/package")
+
+  expect_error(rmd_metadata("document.Rmd"), "does not contain `output: html_notebook`")
+  expect_error(rmd_metadata("minimal-document.Rmd"), "does not contain `output: html_notebook`")
+})
+
+test_that("rmd_metadata errors when output contains an unexpected object type", {
+  mockery::stub(rmd_metadata, "desc::desc_get_urls", "https://example.github.io/package")
+  bad_object <- list(title = "Minimal Notebook", date = "2022-01-22", output = 42)
+  mockery::stub(rmd_metadata, "rmarkdown::yaml_front_matter", bad_object)
+
+  expect_error(rmd_metadata("minimal.Rmd"), "unexpected object type for output")
+})
+
+test_that("rmd_metadata returns correct description with extra spaces", {
+  mockery::stub(rmd_metadata, "desc::desc_get_urls", "https://example.github.io/package")
+  valid <- list(
+    title = "Extra Spaces Notebook", url = "https://example.github.io/package/extra-spaces.html",
+    date = "2022-01-28",
+    description = paste0(
+      "Valid Notebook from 'Analysis Notebook' template ",
+      "with extra spaces before the description line."
+    )
+  )
+
+  expect_identical(rmd_metadata("extra-spaces.Rmd"), valid)
+})
+
+test_that("rmd_metadata errors when DESCRIPTION doesn't contain a URL", {
+  mockery::stub(rmd_metadata, "desc::desc_get_urls", character(0))
+
+  expect_error(rmd_metadata("valid.Rmd"), "no URL found in DESCRIPTION")
+})
+
+test_that("rmd_metadata returns analysis notebook metadata", {
+  mockery::stub(rmd_metadata, "desc::desc_get_urls", "https://example.github.io/package")
+
+  valid <- list(
+    title = "Valid Notebook", url = "https://example.github.io/package/valid.html",
+    date = "2022-01-22", description = "Valid Notebook from 'Analysis Notebook' template."
+  )
+
+  expect_identical(rmd_metadata("valid.Rmd"), valid)
+})
