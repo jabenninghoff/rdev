6b2af1707bf9f359f11191e46594660f4c4ed0f6
John Benninghoff <jbenninghoff@mac.com>
2022-04-03 15:45:56 -0500

    Install test-spelling.R template in use_spelling

---
+++ inst/templates/test-spelling.R
@@ -0,0 +1,33 @@
+# adapted from https://github.com/ropensci/spelling/blob/master/R/spell-check.R
+pkg_dir <- list.files("../../00_pkg_src", full.names = TRUE)
+if (!length(pkg_dir)) {
+  # This is where it is on e.g. win builder
+  check_dir <- dirname(dirname(getwd()))
+  if (grepl("\\.Rcheck$", check_dir)) {
+    source_dir <- sub("\\.Rcheck$", "", check_dir)
+    if (file.exists(source_dir)) {
+      pkg_dir <- source_dir
+    }
+  }
+}
+if (!length(pkg_dir) && identical(basename(getwd()), "testthat")) {
+  if (file.exists("../../DESCRIPTION")) {
+    pkg_dir <- dirname(dirname(getwd()))
+  }
+}
+
+test_that("Package has no spelling errors", {
+  skip_on_cran()
+  withr::local_dir(pkg_dir)
+  expect_identical(spelling::spell_check_package()$word, character(0))
+})
+
+test_that("Notebooks have no spelling errors", {
+  skip_on_cran()
+  withr::local_dir(pkg_dir)
+  if (fs::dir_exists("analysis")) {
+    expect_identical(rdev::spell_check_notebooks()$word, character(0))
+  } else {
+    expect_error(rdev::spell_check_notebooks(), "'analysis' directory not found")
+  }
+})
