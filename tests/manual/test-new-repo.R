192b354c561509d8212c3a32370467094d314967
John Benninghoff <jbenninghoff@mac.com>
2022-04-06 22:40:09 -0500

    Update test-new-repo check 1, add check 2

--- tests/manual/test-new-repo.R
+++ tests/manual/test-new-repo.R
@@ -1,5 +1,8 @@
 # Test script to validate creating a new rdev package or R analysis package
-# Create repository and run this script from the new project
+# Create repository and source this script from the new project
+
+repo_name <- "rdtest1"
+repo_desc <- "rdev test package 1"
 
 # validate that repository is created and opened in RStudio, GitHub Desktop, and default browser
 preconditions <- c(
@@ -8,17 +11,69 @@ preconditions <- c(
   "- Running in a session with no project open",
   "When:",
   "- Creating a new GitHub repository with the following command:",
-  '  `rdev::create_github_repo("rdtest1", "rdev test package 1")`',
+  paste0('  `rdev::create_github_repo("', repo_name, '", "', repo_desc, '")`'),
   "Then:",
-  '- A new rdev package named "rdtest1" is created in the active GitHub account, downloaded to the',
+  paste0(
+    '- A new rdev package named "', repo_name,
+    '" is created in the active GitHub account, downloaded to the'
+  ),
   "  local system, and populated with package files using usethis options in `~/.Rprofile`",
   "- The repository is opened as a new project in RStudio",
   "- On macOS, the repository is opened in GitHub Desktop",
   "- On macOS, the repository settings on github.com are opened in the default browser",
-  "- Re-running the create_github_repo command returns an error"
+  "- Re-running the create_github_repo command returns an error\n"
 )
 writeLines(preconditions)
-writeLines("\nManually validated?")
+writeLines("Manually validated?")
 if (utils::menu(c("Yes", "No")) != 1) {
   stop("terminating script")
 }
+writeLines("\nValidated:")
+writeLines("1. repository is created and opened in RStudio, GitHub Desktop, and default browser")
+
+# validate branch protection, dependabot settings, git commits, git status
+# assume the repo created under the currently logged in user
+gh_login <- gh::gh_whoami()$login
+# assume that only the default branch is returned because the repo was just created
+gh_branches <- gh::gh("GET /repos/{owner}/{repo}/branches", owner = gh_login, repo = repo_name)
+stopifnot(
+  gh_branches[[1]]$protected == TRUE,
+  identical(
+    gh_branches[[1]]$protection,
+    list(
+      enabled = TRUE,
+      required_status_checks = list(
+        enforcement_level = "non_admins",
+        contexts = list("lint", "macOS-latest (release)", "windows-latest (release)"),
+        checks = list(
+          list(context = "lint", app_id = 15368L),
+          list(context = "macOS-latest (release)", app_id = 15368L),
+          list(context = "windows-latest (release)", app_id = 15368L)
+        )
+      )
+    )
+  ),
+  # returns 404 if vulnerability-alerts are not enabled
+  gh::gh(
+    "GET /repos/{owner}/{repo}/vulnerability-alerts",
+    owner = gh_login,
+    repo = repo_name
+  ) == "",
+  # no method to check if automated-security-fixes are enabled
+  nrow(gert::git_log()) == 1,
+  gert::git_log()$message == "Initial commit",
+  gert::git_ls()$path == c(".gitignore", "LICENSE"),
+  identical(
+    gert::git_status(),
+    structure(
+      list(
+        file = c(".Rbuildignore", "DESCRIPTION", "NAMESPACE", "rdtest1.Rproj"),
+        status = c("new", "new", "new", "new"),
+        staged = c(FALSE, FALSE, FALSE, FALSE)
+      ),
+      row.names = c(NA, -4L),
+      class = c("tbl_df", "tbl", "data.frame")
+    )
+  )
+)
+writeLines("2. branch protection, dependabot settings, git commits, git status")
