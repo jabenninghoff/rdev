b326e8c2bcc7bf76b85bb1a42e8b6a109d10a843
John Benninghoff <jbenninghoff@mac.com>
2022-05-02 11:11:45 -0500

    lintr 1088 fixed by 1107

--- tests/testthat/test-release.R
+++ tests/testthat/test-release.R
@@ -374,14 +374,13 @@ test_that("merge_release errors when expected and returns list", {
   gh_merge <- list(merged = TRUE)
   gh <- function(command, ...) {
     if (command == "GET /repos/{owner}/{repo}/pulls") {
-      # object_usage_linter shouldn't trigger here, see https://github.com/r-lib/lintr/issues/1088
-      return(gh_pulls) # nolint: object_usage_linter
+      return(gh_pulls)
     }
     if (command == "GET /repos/{owner}/{repo}/pulls/{pull_number}") {
-      return(gh_pull_number) # nolint: object_usage_linter
+      return(gh_pull_number)
     }
     if (command == "PUT /repos/{owner}/{repo}/pulls/{pull_number}/merge") {
-      return(gh_merge) # nolint: object_usage_linter
+      return(gh_merge)
     }
   }
 
