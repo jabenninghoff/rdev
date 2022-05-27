32a95d780ee93153f373fd351c4d940e42421754
John Benninghoff <jbenninghoff@mac.com>
2022-05-27 07:37:06 -0500

    Add .datatable.aware = TRUE

--- R/package.R
+++ R/package.R
@@ -1,6 +1,13 @@
 #' @keywords internal
 "_PACKAGE"
 
+# Per https://rdatatable.gitlab.io/data.table/articles/datatable-importing.html, rdev must be
+# data.table aware. Otherwise, running build_analysis_site() with notebooks containing data.table
+# will fail with errors described in the links below:
+# https://stackoverflow.com/questions/30601332/data-table-assignment-by-reference-within-function
+# https://stackoverflow.com/questions/10527072/using-data-table-package-inside-my-own-package
+.datatable.aware <- TRUE # nolint: object_name_linter
+
 # Suppress R CMD check note
 #' @importFrom markdown renderMarkdown
 #' @importFrom miniUI miniPage
