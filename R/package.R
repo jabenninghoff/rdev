5224b0a0f1500d4d4b7c60698a5cd7d80473103e
John Benninghoff <jbenninghoff@mac.com>
2022-05-27 08:43:24 -0500

    Update comments with additional research

--- R/package.R
+++ R/package.R
@@ -6,6 +6,9 @@
 # will fail with errors described in the links below:
 # https://stackoverflow.com/questions/30601332/data-table-assignment-by-reference-within-function
 # https://stackoverflow.com/questions/10527072/using-data-table-package-inside-my-own-package
+# https://github.com/Rdatatable/data.table/blob/master/R/cedta.R explicitly allows packages like
+# knitr and rmarkdown that run user code. One option would be to add rdev to cedta.pkgEvalsUserCode,
+# but it's easier and simpler to just use .datatable.aware here.
 .datatable.aware <- TRUE # nolint: object_name_linter
 
 # Suppress R CMD check note
