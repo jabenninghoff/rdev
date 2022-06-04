771d216f3944602b63c65ad22e15f55c4222cb36
John Benninghoff <jbenninghoff@mac.com>
2022-06-04 10:54:41 -0500

    Add test to compare templates to local package

---
+++ tests/testthat/test-inst.R
@@ -0,0 +1,12 @@
+test_that("inst/templates match rdev package locally", {
+  skip_on_ci()
+  skip_on_cran()
+  skip_on_covr()
+  withr::local_dir("../..")
+  expect_identical(readLines("inst/templates/lintr"), readLines(".lintr"))
+  expect_identical(readLines("inst/templates/spelling.R"), readLines("tests/spelling.R"))
+  expect_identical(readLines("inst/templates/pre-commit"), readLines(".git/hooks/pre-commit"))
+  expect_identical(
+    readLines("inst/templates/test-spelling.R"), readLines("tests/testthat/test-spelling.R")
+  )
+})
