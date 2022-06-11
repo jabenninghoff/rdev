aeec0f9cbcb4c9a65c2f8b140ca4df3352c995dc
John Benninghoff <jbenninghoff@mac.com>
2022-06-11 18:27:35 -0500

    Add function to replace setup-analysis script

--- R/init.R
+++ R/init.R
@@ -22,6 +22,50 @@ init <- function() {
   gert::git_branch_create("package-setup")
   gert::git_add(".")
   gert::git_commit("rdev::create_github_repo()")
+
   writeLines("use_rdev_package()...")
   use_rdev_package()
+}
+
+#' Set up analysis package
+#'
+#' Set up an analysis package within an rdev package newly initialized with [init()], by committing
+#'   and running [use_analysis_package()], [use_spelling()], and [ci()].
+#'
+#' `setup_analysis()` will stop if [rlang::is_interactive()] is `FALSE`.
+#'
+#' @export
+setup_analysis <- function() {
+  if (!rlang::is_interactive()) stop("setup_analysis() must be run interactively")
+
+  continue <- utils::askYesNo(
+    "Set up analysis package (run after init)?",
+    default = FALSE
+  )
+  if (is.na(continue) || !continue) {
+    writeLines("Exiting...")
+    return(invisible())
+  }
+
+  writeLines("Committing...")
+  gert::git_add(".")
+  gert::git_commit("rdev::use_rdev_package()")
+
+  writeLines("use_analysis_package()...")
+  use_analysis_package()
+
+  writeLines("Committing...")
+  gert::git_add(".")
+  gert::git_commit("rdev::use_analysis_package()")
+
+  writeLines("use_spelling()...")
+  use_spelling()
+
+  writeLines("Committing...")
+  fs::file_delete("tests/testthat/test-package.R")
+  gert::git_add(".")
+  gert::git_commit("rdev::use_spelling()")
+
+  writeLines("ci()...")
+  ci()
 }
--- tests/testthat/test-init.R
+++ tests/testthat/test-init.R
@@ -31,3 +31,46 @@ test_that("init only runs when askYesNo is explicitly answered 'yes'", {
     "^Creating new branch\\.\\.\\.\\\nuse_rdev_package\\(\\)\\.\\.\\.$"
   )
 })
+
+# setup_analysis
+
+test_that("setup_analysis errors if not running interactively", {
+  mockery::stub(setup_analysis, "gert::git_add", NULL)
+  mockery::stub(setup_analysis, "gert::git_commit", NULL)
+  mockery::stub(setup_analysis, "use_analysis_package", NULL)
+  mockery::stub(setup_analysis, "use_spelling", NULL)
+  mockery::stub(setup_analysis, "fs::file_delete", NULL)
+  mockery::stub(setup_analysis, "ci", NULL)
+  mockery::stub(setup_analysis, "utils::askYesNo", FALSE)
+
+  expect_output(rlang::with_interactive(setup_analysis(), value = TRUE), "^Exiting\\.\\.\\.$")
+  expect_error(
+    rlang::with_interactive(setup_analysis(), value = FALSE),
+    "^setup_analysis\\(\\) must be run interactively$"
+  )
+})
+
+test_that("setup_analysis only runs when askYesNo is explicitly answered 'yes'", {
+  mockery::stub(setup_analysis, "gert::git_add", NULL)
+  mockery::stub(setup_analysis, "gert::git_commit", NULL)
+  mockery::stub(setup_analysis, "use_analysis_package", NULL)
+  mockery::stub(setup_analysis, "use_spelling", NULL)
+  mockery::stub(setup_analysis, "fs::file_delete", NULL)
+  mockery::stub(setup_analysis, "ci", NULL)
+
+  mockery::stub(setup_analysis, "utils::askYesNo", FALSE)
+  expect_output(rlang::with_interactive(setup_analysis(), value = TRUE), "^Exiting\\.\\.\\.$")
+
+  mockery::stub(setup_analysis, "utils::askYesNo", NA)
+  expect_output(rlang::with_interactive(setup_analysis(), value = TRUE), "^Exiting\\.\\.\\.$")
+
+  mockery::stub(setup_analysis, "utils::askYesNo", TRUE)
+  expect_output(
+    rlang::with_interactive(setup_analysis(), value = TRUE),
+    paste0(
+      "^Committing\\.\\.\\.\\\nuse_analysis_package\\(\\)\\.\\.\\.\\\n",
+      "Committing\\.\\.\\.\\\nuse_spelling\\(\\)\\.\\.\\.\\\n",
+      "Committing\\.\\.\\.\\\nci\\(\\)\\.\\.\\.$"
+    )
+  )
+})
