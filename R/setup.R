29345e6ea8d20ce02afb3abcd8f998cf6c00706e
John Benninghoff <jbenninghoff@mac.com>
2023-12-16 21:29:40 -0600

    Use destdir parameter

--- R/setup.R
+++ R/setup.R
@@ -622,7 +622,7 @@ use_analysis_package <- function(use_quarto = TRUE, prompt = FALSE) {
 use_rdev_pkgdown <- function(config_file = "_pkgdown.yml", destdir = "docs") {
   usethis::use_pkgdown(config_file = config_file, destdir = destdir)
   usethis::use_package("pkgdown", type = "Suggests")
-  fs::dir_create(c("pkgdown", "docs"))
+  fs::dir_create(c("pkgdown", destdir))
   usethis::use_template("extra.css", save_as = "pkgdown/extra.css", package = "rdev")
   pkg <- yaml::read_yaml(config_file)
   urls <- desc::desc_get_urls()
