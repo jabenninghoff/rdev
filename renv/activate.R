1614833a7b6b825a4a22a1e7a42fd997ccafcfa7
John Benninghoff <jbenninghoff@mac.com>
2026-03-25 18:56:20 -0500

    renv::update()

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.1.7"
-  attr(version, "md5") <- "dd5d60f155dadff4c88c2fc6680504b4"
+  version <- "1.2.0"
+  attr(version, "md5") <- "b7d230b07507f361d3bcf794d157a188"
   attr(version, "sha") <- NULL
 
   # the project directory
